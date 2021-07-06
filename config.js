924c8391f5ff8bed72da874c242ad54888fcd316
vuvyetanh-25 <87005276+vuvyetanh-25@users.noreply.github.com>
2021-07-06 23:05:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const CONFIG = {
     btnYes: ' được, mai Uyên ôm <33',
     btnNo: 'Không, dmm :33',
     question:'mỗi ôm thôi ư, thêm gì nữa đi chứ :333',
-    btnReply: 'đây nhớ <3',
-    reply: 'thêm cái thơm nữa nè <33333333',
-    mess: 'vietanh đồng ý 🥰. Yêu Uyên nhiều nhiều 😘😘',
+    btnReply: 'ngại quá <3',
+    reply: 'thêm cái thơm nữa được không <3333333333333333333333333333',
+    mess: 'được 🥰. Yêu Uyên nhiều nhiều 😘😘',
     messDesc: 'Uyên nhớ giữ lời hứa đó 🥰.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/vuvietanh.isme/' //link mess của các bạn. VD: https://m.me/nam.nodemy
